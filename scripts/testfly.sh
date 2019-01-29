7d65ab4fccdbe818e9f5bc9843d3a0d2c875e62e
guangliang0209 <fgliang641397267@163.com>
2019-01-29 14:46:30 +0800

    Update testfly.sh

--- scripts/testfly.sh
+++ scripts/testfly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "hello start";
-curl -Lo fly https://github.com/concourse/concourse/releases/download/v2.5.0/fly_darwin_amd64 && chmod +x fly && mv fly /usr/local/bin/
+fly -t manulife-ci pipelines
 echo "end";
