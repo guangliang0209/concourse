7c4c179a8ec952ea8dfba56935a0d82725a59ced
guangliang0209 <fgliang641397267@163.com>
2019-01-30 13:58:47 +0800

    Update testfly.sh

--- scripts/testfly.sh
+++ scripts/testfly.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "hello start>>>>>>>>>>>>>";
-if which npm 2>/dev/null; then
-  echo "npm exists!"
-else
-  echo "npm, no npm installed."
-fi
+#if which npm 2>/dev/null; then
+#  echo "npm exists!"
+#else
+#  echo "npm, no npm installed."
+#fi
+date "+%Y-%m-%d %H:%M:%S";
 echo "hello end<<<<<<<<<<<<<<<";
