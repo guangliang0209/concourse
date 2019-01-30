14403fe116c07fef5b5a9c9f06e3a27096618c65
guangliang0209 <fgliang641397267@163.com>
2019-01-30 10:07:32 +0800

    Update testfly.sh

--- scripts/testfly.sh
+++ scripts/testfly.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo "hello start>>>>>>>>>>>>>";
 curl http://baidu.com/
-###curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-###curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-###curl_setopt($ch, CURLOPT_SSLVERSION, 1);
-###curl -Lo fly https://github.com/concourse/concourse/releases/download/v2.5.0/fly_darwin_amd64 && chmod +x fly && mv fly /usr/local/bin/
-###fly -t manulife-ci pipelines;
 echo "hello end<<<<<<<<<<<<<<<";
