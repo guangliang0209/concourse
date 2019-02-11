00b2f5131574cec086526afbdac5246fbf2244cd
guangliang0209 <fgliang641397267@163.com>
2019-02-11 10:33:30 +0800

    Update testfly.sh

--- scripts/testfly.sh
+++ scripts/testfly.sh
@@ -5,6 +5,7 @@ echo "hello start>>>>>>>>>>>>>";
 #else
 #  echo "npm, no npm installed."
 #fi
-npm install fly
+npm install fly;
+curl -Lo fly https://github.com/concourse/concourse/releases/download/v2.5.0/fly_darwin_amd64 && chmod +x fly && mv fly /usr/local/bin/;
 date "+%Y-%m-%d %H:%M:%S";
 echo "hello end<<<<<<<<<<<<<<<";
