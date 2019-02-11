acf695ac04f3a91e44fa83c635f6973f2d3b732f
guangliang0209 <fgliang641397267@163.com>
2019-02-11 10:54:23 +0800

    Update testfly.sh

--- scripts/testfly.sh
+++ scripts/testfly.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "hello start>>>>>>>>>>>>>";
-#if which npm 2>/dev/null; then
-#  echo "npm exists!"
-#else
-#  echo "npm, no npm installed."
-#fi
+if which npm 2>/dev/null; then
+  echo "npm exists!"
+else
+  echo "npm, no npm installed."
+fi
 npm install fly;
-curl -Lo fly https://github.com/concourse/concourse/releases/download/v2.5.0/fly_darwin_amd64 && chmod +x fly && mv fly /usr/local/bin/;
+#curl -Lo fly https://github.com/concourse/concourse/releases/download/v2.5.0/fly_darwin_amd64 && chmod +x fly && mv fly /usr/local/bin/;
 date "+%Y-%m-%d %H:%M:%S";
 echo "hello end<<<<<<<<<<<<<<<";
